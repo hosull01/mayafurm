820d93226f144eff4649366b2af2895f02e0768b
Harry O'Sullivan <harryosullivan94@hotmail.com>
2019-06-04 23:45:25 +0900

    Adding futher changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Home from './views/Home';
 import Resume from './views/Resume';
 import Modeling from './views/Modeling';
 import Contact from './views/Contact';
@@ -31,14 +32,15 @@ function App() {
           <div className="main">
             <Route render={({location}) => (
               <TransitionGroup>
-              <CSSTransition key={location.key} timeout={300} classNames="fade" >
-                <Switch location={location}>
-                  <Route exact path="/resume" component={Resume} />
-                  <Route exact path="/modeling" component={Modeling} />
-                  <Route exact path="/contact" component={Contact} />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
+                <CSSTransition key={location.key} timeout={300} classNames="fade" >
+                  <Switch location={location}>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/resume" component={Resume} />
+                    <Route exact path="/modeling" component={Modeling} />
+                    <Route exact path="/contact" component={Contact} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
             )} />        
           </div>
         </div>
